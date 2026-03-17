db52a2c673cceb830f130b9f4e2bf97d8fa07ab2
Ihor Zasimenko <boxletter2011@gmail.com>
2026-03-17 20:10:28 +0200

    Add main.py, update Dockerfile, requirements and dockerignore

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+import os
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise ValueError("API_KEY environment variable not set")
+
+    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q=Paris&aqi=no"
+    response = requests.get(url)
+    response.raise_for_status()  # якщо буде помилка HTTP
+
+    data = response.json()
+    city = data["location"]["name"]
+    temp_c = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+
+    print(f"Weather in {city}: {temp_c}°C, {condition}")
 
 
 if __name__ == "__main__":
